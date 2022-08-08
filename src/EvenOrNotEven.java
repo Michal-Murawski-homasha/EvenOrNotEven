2c641820f8c3607e0446fc3880f073fba19a8751
Michał Murawski <homasha@gmail.com>
2022-08-08 16:25:20 +0200

    Initial commit

---
+++ src/EvenOrNotEven.java
@@ -0,0 +1,14 @@
+public class EvenOrNotEven {
+    public static void main(String[] args) {
+        int i = 10;
+        EvenOrNotEvenNumber(i);
+    }
+
+    private static void EvenOrNotEvenNumber(int i) {
+        if (i % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Not even");
+        }
+    }
+}
